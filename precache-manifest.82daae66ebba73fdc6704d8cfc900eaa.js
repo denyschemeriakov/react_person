3c44e062970d9ff4d1f16ac0b327778f9cfcd347
Denys Chemeriakov <chemeriakov@gmail.com>
2021-08-31 20:52:03 +0300

    Updates

---
+++ precache-manifest.82daae66ebba73fdc6704d8cfc900eaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "445fa0bdee4805dddc227d714dafd25a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5003e9c8580c99c4e15d",
+    "url": "./static/css/main.ae4f840b.chunk.css"
+  },
+  {
+    "revision": "bef14ca31350064182c6",
+    "url": "./static/js/2.e8d9cca5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e8d9cca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5003e9c8580c99c4e15d",
+    "url": "./static/js/main.85f126d6.chunk.js"
+  },
+  {
+    "revision": "e447b2ddaa3314191b11",
+    "url": "./static/js/runtime-main.e90a3edd.js"
+  }
+]);
